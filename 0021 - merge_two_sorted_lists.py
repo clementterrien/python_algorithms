1895f3ff95c62e8a97bbc7f9728ffc62c0038eb6
Clément Terrien <clement.terrien@hotmail.fr>
2025-01-21 11:09:50 +0100

    21 Updating with the use of common ListNode

--- 0021 - merge_two_sorted_lists.py
+++ 0021 - merge_two_sorted_lists.py
@@ -1,75 +1,13 @@
 # This is a problem given by Leetcode.com
 # https://leetcode.com/problems/merge-two-sorted-lists/description/
 
-from typing import Any, Generator
 from unittest import TestCase, main
 
+from utils.list_node import ListNode
 
-class ListNode:
-    def __init__(self, val=0, next=None) -> None:
-        self.val = val
-        self.next = next
 
-    def __str__(self) -> str:
-        return str(self.val)
-
-    def __eq__(self, value: object) -> bool:
-        if not isinstance(value, list):
-            return super().__eq__(value)
-        else:
-            return list(self) == value
-
-    def __iter__(self) -> Generator[int, Any, None]:
-        current = self
-        while current:
-            yield current.val
-            current = current.next
-
-    def __repr__(self) -> str:
-        return str(self.val)
-
-    def generate_list_node_from_int_list(self, nums: list[int]) -> "ListNode":
-        """ Generates a linked list from a given list of integers, using the current `ListNode` as the head.
-
-            Args:
-                nums (list[int]): A list of integers to populate the linked list. The first integer
-                                will be assigned to the current node (`self`), and subsequent integers
-                                will form the rest of the linked list.
-
-            Returns:
-                ListNode: The head of the constructed linked list (i.e., the current node `self`).
-
-            Example:
-                If `nums = [1, 2, 3]` and the current `ListNode` is the head:
-
-                Initial state:
-                    self = ListNode(val=0)
-
-                After calling `generate_list_node_from_int_list(nums)`:
-                    self = ListNode(val=1) -> ListNode(val=2) -> ListNode(val=3)
-
-            Notes:
-                - The method modifies the `self` node and appends new nodes to it.
-                - Assumes that `nums` is not empty. If `nums` is empty, it will raise an `IndexError`
-                when trying to access `nums[0]`.
-                - The resulting linked list will follow the order of integers in `nums`.
-
-            Raises:
-                ValueError: If given nums is empty or contains non integer elements.
-        """
-        if not isinstance(nums, list) and not all([isinstance(x, int) for x in nums]):
-            raise ValueError("nums must be a list of integers.")
-        if not nums:
-            raise ValueError("nums can't be empty.")
-
-        current = self
-        self.val = nums[0]
-
-        for num in nums[1:]:
-            current.next = ListNode(val=num)
-            current = current.next
-
-        return self
+ListNode.min_val = -100
+ListNode.max_val = 100
 
 
 class Solution:
